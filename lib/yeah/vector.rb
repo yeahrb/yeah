962a35079dc24e9ecfb00fe490488cf3720bf583
Artur Ostrega <artur@ostrega.me>
2013-06-16 00:31:09 -0500

    for Vector, extended ::[] and started on #components

--- lib/yeah/vector.rb
+++ lib/yeah/vector.rb
@@ -1,5 +1,13 @@
 class Yeah::Vector
-  def self.[]
-    self.new
+  def initialize(*components)
+    @components = components.empty? ? [0, 0, 0] : components
+  end
+
+  def self.[](*components)
+    self.new(*components)
+  end
+
+  def components
+    @components
   end
 end
--- spec/vector_spec.rb
+++ spec/vector_spec.rb
@@ -9,5 +9,33 @@ describe Yeah::Vector do
     it "instantiates" do
       Yeah::Vector[].should be_instance_of Yeah::Vector
     end
+
+    it "with no parameters instantiates with components of [0, 0, 0]" do
+      vector = Yeah::Vector[]
+      vector.components.should eql [0, 0, 0]
+    end
+
+    it "with parameters 4, 5, 6 instantiates with components of [4, 5, 6]" do
+      vector = Yeah::Vector[4, 5, 6]
+      vector.components.should eql [4, 5, 6]
+    end
+  end
+
+  describe '#components' do
+    before do
+      @vector = Yeah::Vector[]
+    end
+
+    it "is an Array" do
+      @vector.components.should be_instance_of Array
+    end
+
+    it "has 3 Numeric elements" do
+      @vector.components.should have(3).elements
+
+      @vector.components.each do |c|
+        c.should be_a Numeric
+      end
+    end
   end
 end
