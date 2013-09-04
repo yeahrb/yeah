7d195dfbf75250e9efa9d9d333ca82dbe6453bdf
Artur Ostrega <amo@lavabit.com>
2013-09-04 18:25:07 -0500

    Entity's internals work with instance variables, not public interfaces to them

--- lib/yeah/entity.rb
+++ lib/yeah/entity.rb
@@ -2,30 +2,30 @@ class Yeah::Entity
   attr_accessor :position
 
   def initialize(*position)
-    self.position = Yeah::Vector[*position]
+    @position = Yeah::Vector[*position]
   end
 
   def x
-    position.x
+    @position.x
   end
 
   def x=(value)
-    self.position.x = value
+    @position.x = value
   end
 
   def y
-    position.y
+    @position.y
   end
 
   def y=(value)
-    self.position.y = value
+    @position.y = value
   end
 
   def z
-    position.z
+    @position.z
   end
 
   def z=(value)
-    self.position.z = value
+    @position.z = value
   end
 end
