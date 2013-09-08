0e7d30984acef6c3ce278ac1c9d3eea9395f0ccf
Artur Ostrega <skoofoo@gmail.com>
2013-09-08 16:52:59 -0500

    Color is now a Class

--- lib/yeah/color.rb
+++ lib/yeah/color.rb
@@ -1,10 +1,11 @@
-module Yeah::Color
-  def self.byte_array(from)
-    case from
-    when Array
-      from
-    when String
-      from.scan(/.{2}/).map { |hex| hex.to_i(16) }
-    end
+class Yeah::Color
+  attr_accessor :byte_array
+
+  class << self
+    alias_method :[], :new
+  end
+
+  def initialize(value=[0, 0, 0, 255])
+    @byte_array = value
   end
 end
--- spec/color_spec.rb
+++ spec/color_spec.rb
@@ -1,16 +1,35 @@
 require 'spec_helper'
 
 describe Yeah::Color do
-  let(:modjul) { Yeah::Color }
+  let(:klass) { Yeah::Color }
+  let(:instance) { klass.new }
 
-  it { modjul.should be_kind_of Module }
+  it { klass.should be_kind_of Class }
 
-  describe '::byte_array' do
-    subject(:method) { modjul.method(:byte_array) }
+  [:new, :[]].each do |method_name|
+    describe "::#{method_name}" do
+      subject(:method) { klass.method(method_name) }
 
-    it { method.call([10, 20, 30, 40]).should eq [10, 20, 30, 40] }
-    it { method.call([255, 255, 255, 255]).should eq [255, 255, 255, 255] }
-    it { method.call('00FF01FE').should eq [0, 255, 1, 254] }
-    it { method.call('11223344').should eq [17, 34, 51, 68] }
+      it { method.call.should be_instance_of klass }
+      it { method.call.byte_array.should eq [0, 0, 0, 255] }
+      it { method.call([10, 20, 30, 40]).byte_array.should eq [10, 20, 30, 40] }
+      it { method.call([55, 25, 55, 25]).byte_array.should eq [55, 25, 55, 25] }
+    end
+  end
+
+  describe '#byte_array' do
+    subject(:byte_array) { instance.byte_array }
+
+    it { should eq [0, 0, 0, 255] }
+  end
+
+  describe '#byte_array=' do
+    subject(:method) { instance.method(:byte_array=) }
+
+    it "assigns #byte_array" do
+      byte_array = (1..4).map { Random.rand(255) }
+      method.call byte_array
+      instance.byte_array.should eq byte_array
+    end
   end
 end
