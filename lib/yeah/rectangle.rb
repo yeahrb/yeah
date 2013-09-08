f72d591c0858b046f1057dcdf88bb502f933a51d
Artur Ostrega <skoofoo@gmail.com>
2013-09-08 16:30:37 -0500

    simplified Rectangle#size/#color

--- lib/yeah/rectangle.rb
+++ lib/yeah/rectangle.rb
@@ -1,22 +1,8 @@
 class Yeah::Rectangle
+  attr_accessor :size, :color
+
   def initialize(size=Yeah::Vector[], color=[255]*4)
     @size = size
     @color = color
-  end
-
-  def size
-    @size
-  end
-
-  def size=(value)
-    @size = value
-  end
-
-  def color
-    @color
-  end
-
-  def color=(value)
-    @color = value
   end
 end
