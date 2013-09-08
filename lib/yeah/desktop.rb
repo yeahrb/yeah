0a19329baf76c5e23c028b5fb3e62b832795b444
Artur Ostrega <skoofoo@gmail.com>
2013-09-07 20:51:49 -0500

    Desktop @screen

--- lib/yeah/desktop.rb
+++ lib/yeah/desktop.rb
@@ -1,2 +1,7 @@
+require 'rubygame'
+
 class Yeah::Desktop
+  def initialize
+    @screen = Rubygame::Screen.new([320, 240])
+  end
 end
--- spec/desktop_spec.rb
+++ spec/desktop_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 describe Yeah::Desktop do
   let(:klass) { Yeah::Desktop }
+  let(:instance) { klass.new }
 
   it { klass.should be_instance_of Class }
+
+  describe '::new' do
+
+    it { instance.instance_variables.should include :@screen }
+
+    describe '@screen' do
+      subject(:screen) { instance.instance_variable_get(:@screen) }
+
+      it { screen.should be_instance_of Rubygame::Screen }
+      it { screen.size.should eq [320, 240] }
+    end
+  end
 end
