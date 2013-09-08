b9089705ef49e0ff9f71bc063b56d4545fe313bd
Artur Ostrega <skoofoo@gmail.com>
2013-09-08 01:06:15 -0500

    removed Yeah::Rectangle initialization argument check, renamed shape_spec.rb to rectangle_spec.rb

--- lib/yeah/rectangle.rb
+++ lib/yeah/rectangle.rb
@@ -1,6 +1,2 @@
 class Yeah::Rectangle
-  def initialize(*arguments)
-    err_msg = "too few arguments (#{arguments.count} for at least 1)"
-    raise ArgumentError, err_msg
-  end
 end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -2,15 +2,13 @@ require 'spec_helper'
 
 describe Yeah::Rectangle do
   let(:klass) { Yeah::Rectangle }
+  let(:instance) { klass.new }
 
   it { klass.should be_kind_of Class }
 
   describe '::new' do
     subject(:method) { klass.method(:new) }
 
-    it "complains without arguments" do
-      expect { method.call }.
-        to raise_error ArgumentError, /too few arguments/
-    end
+    it { method.call.should be_instance_of Yeah::Rectangle }
   end
 end
