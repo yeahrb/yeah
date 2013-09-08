7304b3b11844399501d74a6d7929d93e709abb04
Artur Ostrega <skoofoo@gmail.com>
2013-09-07 21:10:58 -0500

    start of Shape#initialize

--- lib/yeah/shape.rb
+++ lib/yeah/shape.rb
@@ -1,2 +1,6 @@
 class Yeah::Shape
+  def initialize(*arguments)
+    err_msg = "too few arguments (#{arguments.count} for at least 2)"
+    raise ArgumentError, err_msg
+  end
 end
--- spec/shape_spec.rb
+++ spec/shape_spec.rb
@@ -4,4 +4,13 @@ describe Yeah::Shape do
   let(:klass) { Yeah::Shape }
 
   it { klass.should be_kind_of Class }
+
+  describe '::new' do
+    subject(:method) { klass.method(:new) }
+
+    it "complains without arguments" do
+      expect { method.call }.
+        to raise_error ArgumentError, /too few arguments/
+    end
+  end
 end
