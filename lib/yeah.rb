60234dfa74efaf0fcc064056a030a51c04f9e6e3
Artur Ostrega <skoofoo@gmail.com>
2013-09-12 12:43:22 -0500

    DRYed requires

--- lib/yeah.rb
+++ lib/yeah.rb
@@ -2,11 +2,7 @@ module Yeah
   VERSION = '0.1.0'
 end
 
-require 'yeah/vector'
-require 'yeah/color'
-require 'yeah/surface'
-require 'yeah/rectangle'
-require 'yeah/entity'
-require 'yeah/map'
-require 'yeah/desktop'
-require 'yeah/game'
+requires = %i(vector color surface rectangle entity map desktop game)
+requires.each do |req|
+  require "yeah/#{req}"
+end
