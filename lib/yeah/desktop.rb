0274fcb14c7eefb5fca735550b19dcc8593c224f
Artur Ostrega <skoofoo@gmail.com>
2013-09-11 20:43:54 -0500

    decoupled Desktop from game

--- lib/yeah/desktop.rb
+++ lib/yeah/desktop.rb
@@ -1,10 +1,7 @@
 require 'rubygame'
 
 class Yeah::Desktop
-  attr_accessor :game
-
-  def initialize(game=nil)
-    @game = game
+  def initialize
     @screen = Rubygame::Screen.new([320, 240])
   end
 end
--- spec/desktop_spec.rb
+++ spec/desktop_spec.rb
@@ -9,14 +9,6 @@ describe Yeah::Desktop do
   describe '::new' do
     subject(:method) { klass.method(:new) }
 
-    it { method.call.game.should eq nil }
-
-    it "takes game as first argument" do
-      game = Yeah::Game.new
-      inst = method.call(game)
-      inst.game.should eq game
-    end
-
     it { instance.instance_variables.should include :@screen }
     describe '@screen' do
       subject(:screen) { instance.instance_variable_get(:@screen) }
@@ -24,17 +16,5 @@ describe Yeah::Desktop do
       it { screen.should be_instance_of Rubygame::Screen }
       it { screen.size.should eq [320, 240] }
     end
-  end
-
-  describe '#game' do
-    subject { instance.game }
-
-    it { should eq nil }
-  end
-
-  describe '#game=' do
-    subject { instance.method(:game=) }
-
-    it_behaves_like 'writer', Yeah::Game.new
   end
 end
