902fcba442096ba0e9223f688c31356d19b0ae9b
Artur Ostrega <skoofoo@gmail.com>
2013-09-12 22:01:32 -0500

    Rectangle#draw, Rectangle#color is of type Color

--- lib/yeah/rectangle.rb
+++ lib/yeah/rectangle.rb
@@ -1,8 +1,14 @@
 class Yeah::Rectangle
   attr_accessor :size, :color
 
-  def initialize(size=Yeah::Vector[], color=[255]*4)
+  def initialize(size=Yeah::Vector[], color=Color[*[255]*4])
     @size = size
     @color = color
+  end
+
+  def draw
+    surface = Yeah::Surface.new(size)
+    surface.fill_rectangle(Vector[], size-Vector[1, 1], color)
+    surface
   end
 end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -10,15 +10,18 @@ describe Rectangle do
     subject(:method) { klass.method(:new) }
 
     it { method.call.should be_instance_of klass }
+    it { method.call.size.should eq Vector[] }
+    it { method.call.color.should eq Color[255, 255, 255, 255] }
 
     it "assigns Vector first argument as #size" do
       vector = Vector[Random.rand(40)]
       method.call(vector).size.should eq vector
     end
 
-    it "assigns second argument RGBA byte array as #color" do
+    it "assigns second argument Color as #color" do
       byte_array = (1..4).map { Random.rand(255) }
-      method.call(Vector[], byte_array).color.should eq byte_array
+      color = Color[*byte_array]
+      method.call(Vector[], color).color.should eq color
     end
   end
 
@@ -37,12 +40,32 @@ describe Rectangle do
   describe '#color' do
     subject(:color) { instance.color }
 
-    it { should eq [255, 255, 255, 255] }
+    it { should eq Color[255, 255, 255, 255] }
   end
 
   describe '#color=' do
     subject(:method) { instance.method(:color=) }
 
     it_behaves_like 'writer', [Random.rand(255)]*4
+  end
+
+  describe '#draw' do
+    subject(:method) { instance.method(:draw) }
+
+    its(:call) { should be_instance_of Surface }
+
+    it "matches size" do
+      instance.size = Vector[Random.rand(49)+1, 50]
+      method.call.size.should eq instance.size
+    end
+
+    it "matches color" do
+      instance.size = Vector[10, 10]
+      instance.color = Color[*[Random.rand(255)]*4]
+      surface = method.call
+      surface.color_at(Vector[]).should eq instance.color
+      surface.color_at(instance.size/2).should eq instance.color
+      surface.color_at(instance.size-Vector[1, 1]).should eq instance.color
+    end
   end
 end
