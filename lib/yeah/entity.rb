10a7cdf47aa905add37b9c77f6b957e3c8306db0
Artur Ostrega <skoofoo@gmail.com>
2013-10-09 17:50:21 -0500

    cleaned up Entity

--- lib/yeah/entity.rb
+++ lib/yeah/entity.rb
@@ -1,32 +1,19 @@
 class Yeah::Entity
   attr_accessor :position, :visual
 
+  class << self
+    def define_position_helpers
+      %w(x y z).each_with_index do |coord, i|
+        define_method(coord) { @position[i] }
+        define_method("#{coord}=") { |val| @position[i] = val }
+      end
+    end
+  end
+
+  define_position_helpers
+
   def initialize(position=Vector[])
     @position = position
-  end
-
-  def x
-    @position.x
-  end
-
-  def x=(value)
-    @position.x = value
-  end
-
-  def y
-    @position.y
-  end
-
-  def y=(value)
-    @position.y = value
-  end
-
-  def z
-    @position.z
-  end
-
-  def z=(value)
-    @position.z = value
   end
 
   def draw
