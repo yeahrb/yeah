359b0098ccbbb9b8e27b5944bf285da5f2be109d
Artur Ostrega <skoofoo@gmail.com>
2013-10-29 21:12:14 -0500

    BasicPhysics module

--- lib/yeah.rb
+++ lib/yeah.rb
@@ -4,7 +4,8 @@ end
 
 require 'monkey/numeric'
 
-yeah_requires = %i(vector color surface rectangle entity map desktop game)
+yeah_requires = %i(vector color surface rectangle entity map desktop game
+                   basic_physics)
 yeah_requires.each do |req|
   require "yeah/#{req}"
 end
---
+++ spec/basic_physics_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe BasicPhysics do
+  let(:modjul) { described_class }
+  let(:entity) do
+    Entity.class_eval "include #{modjul.name}"
+    Entity.new
+  end
+
+  it { modjul.should be_kind_of Module }
+
+  describe '#velocity' do
+    subject { entity.velocity }
+
+    it { should eq Vector[0, 0, 0] }
+  end
+
+  describe '#velocity=' do
+    subject { entity.method(:velocity=) }
+
+    it_behaves_like 'writer', Vector[1, 2, 3]
+  end
+
+  describe '#move' do
+    subject(:method) { entity.method(:move) }
+
+    it "adds velocity to position" do
+      entity.velocity = Vector[3, 6, 9]
+      method.call
+      entity.position.should eq entity.velocity
+    end
+  end
+end
