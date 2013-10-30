c668362883dba7813f10c7c572363e5909bef3c7
Artur Ostrega <skoofoo@gmail.com>
2013-10-29 22:19:03 -0500

    Map(::|#)key[=]

--- lib/yeah/map.rb
+++ lib/yeah/map.rb
@@ -3,8 +3,18 @@ class Yeah::Map
   # @!attribute background
   #   @return [Color] background color
   attr_accessor :background
+  attr_writer :key
+
+  def self.key(key)
+    @@key = key
+  end
 
   def initialize
     @background = Color[]
+    @key = @@key || {}
+  end
+
+  def key
+    @key
   end
 end
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -6,15 +6,37 @@ describe Map do
 
   it { klass.should be_instance_of Class }
 
-  describe '#background' do
-    subject(:background) { instance.background }
+  describe '::key' do
+    subject(:method) { klass.method(:key) }
 
+    after(:each) { klass.class_variable_set :@@key, nil }
+
+    it { expect{method.call}.to raise_error ArgumentError }
+
+    it "assigns #key in instances" do
+      key = { '#' => Entity }
+      method.call(key)
+      klass.new.key.should eq key
+    end
+  end
+
+  describe '#background' do
+    subject { instance.background }
     it { should eq Color[] }
   end
 
   describe '#background=' do
     subject(:method) { instance.method(:background=) }
-
     it_behaves_like 'writer', Color[*[Random.rand(255)]*4]
+  end
+
+  describe '#key' do
+    subject { instance.key }
+    it { should eq Hash.new }
+  end
+
+  describe '#key=' do
+    subject(:method) { instance.method(:key=) }
+    it_behaves_like 'writer', { '#' => Entity }
   end
 end
