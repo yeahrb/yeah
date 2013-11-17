fedc6472f71b31b5f99addb04802c3cdf47efc09
Artur Ostrega <skoofoo@gmail.com>
2013-11-16 23:59:11 -0600

    organized Desktop

--- lib/yeah/desktop.rb
+++ lib/yeah/desktop.rb
@@ -2,14 +2,6 @@ require 'rubygame'
 
 # Bindings to the native desktop powered by Rubygame.
 class Yeah::Desktop
-  # @!attribute [r] screen
-  #   @return [Rubygame::Screen]
-  # @!attribute resolution
-  #   @return [Vector] size of game window
-  # @!attribute tickrate
-  #   @return [Integer] target ticks per second
-  attr_reader :screen, :resolution, :tickrate
-
   def initialize(resolution=V[320, 180])
     self.resolution = resolution
 
@@ -22,14 +14,45 @@ class Yeah::Desktop
     pressables_keys.each { |pk| @pressables[pk] = false }
   end
 
+  # @!attribute [r] screen
+  #   @return [Rubygame::Screen]
+  attr_reader :screen
+
+  # @!attribute resolution
+  #   @return [Vector] size of game window
+  attr_reader :resolution
+
   def resolution=(value)
     @screen = Rubygame::Screen.new(value.components[0..1])
     @resolution = value
   end
 
+  # @!attribute tickrate
+  #   @return [Integer] target ticks per second
+  attr_reader :tickrate
+
   def tickrate=(value)
     @clock.target_framerate = value
     @tickrate = value
+  end
+
+  # Is a key or button being pressed?
+  # @param [Symbol|Integer] key or button
+  def pressing?(*pressables)
+    raise ArgumentError if pressables.empty?
+    pressables.any? { |p| @pressables[p] }
+  end
+
+  # Press a key or button.
+  # @param [Symbol|Integer] key or button
+  def press(pressable)
+    @pressables[pressable] = true
+  end
+
+  # Release a key or button.
+  # @param [Symbol|Integer] key or button
+  def release(pressable)
+    @pressables[pressable] = false
   end
 
   # Project a surface onto screen.
@@ -49,24 +72,5 @@ class Yeah::Desktop
       yield
       @clock.tick
     end
-  end
-
-  # Press a key or button.
-  # @param [Symbol|Integer] key or button
-  def press(pressable)
-    @pressables[pressable] = true
-  end
-
-  # Release a key or button.
-  # @param [Symbol|Integer] key or button
-  def release(pressable)
-    @pressables[pressable] = false
-  end
-
-  # Is a key or button being pressed?
-  # @param [Symbol|Integer] key or button
-  def pressing?(*pressables)
-    raise ArgumentError if pressables.empty?
-    pressables.any? { |p| @pressables[p] }
   end
 end
