f8d899e6fad038f365ae715ebfe4a71635ef75ab
Artur Ostrega <skoofoo@gmail.com>
2013-11-16 23:39:41 -0600

    organized Entity

--- lib/yeah/entity.rb
+++ lib/yeah/entity.rb
@@ -1,74 +1,37 @@
 # Game object.
 class Yeah::Entity
-  # @!attribute position
-  #   @return [Vector] position within a game
-  # @!attribute size
-  #   @return [NilClass|Vector] visual size
-  # @!attribute state
-  #   @return [Symbol] state in game
-  # @!attribute visual
-  #   @return [Visual] visual representation within a game
-  # @!attribute game
-  #   @return [Game] game to which this belongs to
-  attr_accessor :position, :state, :visual
-  attr_reader :game
-  attr_writer :size
-
   def initialize(position=V[])
     @position = position
   end
 
-  class << self
-    def define_position_helpers
-      %w(x y z).each_with_index do |coord, i|
-        define_method(coord) { @position[i] }
-        define_method("#{coord}=") { |val| @position[i] = val }
-      end
-    end
-  end
-
-  def size
-    @size || visual && visual.size || V[]
-  end
+  # @!attribute game
+  #   @return [Game] game to which this belongs to
+  attr_reader :game
 
   def game=(value)
     @game = value
     @game.entities << self unless @game.entities.include? self
   end
 
-  # @!attribute x
-  #   @return [Vector] position.x
-  # @!attribute y
-  #   @return [Vector] position.y
-  # @!attribute z
-  #   @return [Vector] position.z
-  define_position_helpers
+  # @!attribute position
+  #   @return [Vector] position within a game
+  attr_accessor :position
 
-  # Update entity.
-  def update
+  # @!attribute size
+  #   @return [NilClass|Vector] visual size
+  def size
+    @size || visual && visual.size || V[]
   end
 
-  # Get visual representation from visual.
-  #   @return [Surface] visual representation
-  def draw
-    visual.draw if visual
-  end
+  attr_writer :size
 
-  def pressing?(pressable)
-    game.pressing? pressable
-  end
+  # @!attribute state
+  #   @return [Symbol] state in game
+  attr_accessor :state
 
-  def control(attrName, input, value)
-    if input.class == Array
-      polarity = 0
-      polarity += 1 if game.platform.pressing?(input.first)
-      polarity -= 1 if game.platform.pressing?(input.last)
-    else
-      polarity = game.platform.pressing?(input) ? 1 : -1
-    end
-
-    self.instance_eval("#{attrName} += #{value} * #{polarity}")
-  end
+  # @!attribute visual
+  #   @return [Visual] visual representation within a game
+  attr_accessor :visual
 
   # X of right edge.
   #   @return [Integer]
@@ -134,4 +97,47 @@ class Yeah::Entity
 
     !(not_touching_x && not_touching_y && not_touching_z)
   end
+
+  def control(attrName, input, value)
+    if input.class == Array
+      polarity = 0
+      polarity += 1 if game.platform.pressing?(input.first)
+      polarity -= 1 if game.platform.pressing?(input.last)
+    else
+      polarity = game.platform.pressing?(input) ? 1 : -1
+    end
+
+    self.instance_eval("#{attrName} += #{value} * #{polarity}")
+  end
+
+  def pressing?(pressable)
+    game.pressing? pressable
+  end
+
+  # Update entity.
+  def update
+  end
+
+  # Get visual representation from visual.
+  #   @return [Surface] visual representation
+  def draw
+    visual.draw if visual
+  end
+
+  class << self
+    def define_position_helpers
+      %w(x y z).each_with_index do |coord, i|
+        define_method(coord) { @position[i] }
+        define_method("#{coord}=") { |val| @position[i] = val }
+      end
+    end
+  end
+
+  # @!attribute x
+  #   @return [Vector] position.x
+  # @!attribute y
+  #   @return [Vector] position.y
+  # @!attribute z
+  #   @return [Vector] position.z
+  define_position_helpers
 end
