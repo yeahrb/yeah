6ee5075d9cc64205bcf3249a0665dd195a0bcddd
Artur Ostrega <skoofoo@gmail.com>
2013-11-17 00:34:55 -0600

    cleaned up Numeric#limit

--- lib/monkey/numeric.rb
+++ lib/monkey/numeric.rb
@@ -1,6 +1,8 @@
 class Numeric
   def limit(value)
-    plus_minus = self <=> 0
-    self.abs > value.abs ? value * plus_minus : self
+    polarity = self > 0 ? +1 : -1
+    value = value * polarity
+
+    self.abs > value.abs ? value : self
   end
 end
