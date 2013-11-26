bcc51e3977bd5301343fc97475a0a3df849fc44b
Artur Ostrega <skoofoo@gmail.com>
2013-11-26 15:49:16 -0600

    removed yugo demo

--- demo/yugo/entities/wall.rb
+++
@@ -1,4 +0,0 @@
-class Wall < Entity
-  visual Rectangle.new(V[16, 16], :brown)
-  # size V[16, 16] # implicit
-end
--- demo/yugo/game.rb
+++
@@ -1,5 +0,0 @@
-require 'yeah'
-
-class YugoGame < Yeah::Game
-  # first_map :training # if unspecified, picks first file in /maps
-end
