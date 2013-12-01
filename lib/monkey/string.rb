f1722bb40410254623543d8f3b1b73f21b65756f
Artur Ostrega <skoofoo@gmail.com>
2013-12-01 16:11:52 -0600

    String#classify

--- lib/monkey/string.rb
+++ lib/monkey/string.rb
@@ -2,4 +2,8 @@ class String
   def unindent
     gsub /^#{self[/\A\s*/]}/, ''
   end
+
+  def classify
+    split('_').collect { |w| w.sub(/\A(.)/){ $1.upcase } }.join
+  end
 end
--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -7,4 +7,9 @@ describe String do
     it { "\tyo\n\t\tbrudda".unindent.should eq "yo\n\tbrudda" }
     it { "ey\n\n\ney guy".unindent.should eq "ey\n\n\ney guy" }
   end
+
+  describe '#classify' do
+    it { "AwesomeClass".classify.should eq "AwesomeClass" }
+    it { "ultra_class".classify.should eq "UltraClass" }
+  end
 end
