0816d28e991c313e64f6c25fc8f1557624df5053
Artur Ostrega <skoofoo@gmail.com>
2014-02-13 15:31:43 -0600

    moved core/ contents to root

--- lib/yeah.rb
+++ lib/yeah.rb
@@ -1,4 +1,16 @@
 module Yeah; end
 
 require 'yeah/version'
-require 'yeah/core'
+
+require 'yeah/null_visual'
+require 'yeah/null_context'
+
+require 'yeah/vector'
+require 'yeah/v'
+require 'yeah/color'
+
+require 'yeah/entity'
+
+require 'yeah/stage'
+
+require 'yeah/game'
