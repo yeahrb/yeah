70b1b97445d7365021513a9ccd417b37237d5e15
Artur Ostrega <skoofoo@gmail.com>
2014-03-09 21:39:46 -0500

    Command#run -> Command#build spec

--- spec/utility/command/build_spec.rb
+++ spec/utility/command/build_spec.rb
@@ -1,13 +1,13 @@
-describe Command, '::run' do
+describe Command, '::build' do
   let(:dir) { '/dev/null/' }
 
   it "loads a project" do
-    project_stub = Struct.new(:run)
+    project_stub = Struct.new(:build)
     Project.should receive(:load).with(dir).and_return(project_stub.new)
-    described_class.run(dir)
+    described_class.build(dir)
   end
 
-  it "runs a project" do
+  it "builds a project" do
     # TODO
   end
 end
