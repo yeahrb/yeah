64e901eea5b7b2da007d5f9c25e74f978ed33c3d
Artur Ostrega <skoofoo@gmail.com>
2014-03-12 19:26:54 -0500

    cleaned Game::new spec

--- spec/game/class/new_spec.rb
+++ spec/game/class/new_spec.rb
@@ -1,8 +1,4 @@
 describe Game, '::new' do
-  it "has NullContext context by default" do
-    described_class.new.context.should be_instance_of NullContext
-  end
-
   it "accepts context" do
     whatever_context = Object.new
     instance = described_class.new(whatever_context)
