fafc64fc13befb748342756e207debc73336e41f
Artur Ostrega <skoofoo@gmail.com>
2014-03-13 18:21:37 -0500

    fixed Level::new spec

--- spec/level/class/new_spec.rb
+++ spec/level/class/new_spec.rb
@@ -3,10 +3,10 @@ describe Level, '::new' do
 
   it "accepts properties hash" do
     properties = {
-      background: [1, 0.5, 1, 0.51]
+      background: [1, 0.5, 1]
     }
     instance = described_class.new(properties)
 
-    expect(instance.background).to eq properties[:background]
+    expect(instance.background.to_a).to eq properties[:background]
   end
 end
