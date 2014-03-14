2b19774ac0bb8db31724fdce698bb41bdcdbc17b
Artur Ostrega <skoofoo@gmail.com>
2014-03-13 21:23:31 -0500

    Level#things= accepts representative hash

--- lib/yeah/level.rb
+++ lib/yeah/level.rb
@@ -25,8 +25,24 @@ class Level
     @things ||= []
   end
   def things=(value)
+    # Convert representative hash to thing instances.
+    if value.respond_to?(:to_h)
+      new_things = []
+
+      value.to_h.each do |type_name, positions|
+        type = Kernel.const_get(type_name)
+        positions = [positions] unless positions.first.respond_to?(:to_a)
+
+        positions.each do |position|
+          new_things << type.new(position)
+        end
+      end
+
+      value = new_things
+    end
+
     @things = value
-    @things.each { |e| e.level = self }
+    @things.each { |t| t.level = self }
   end
 
   attr_reader :game
--- spec/level/things_spec.rb
+++ spec/level/things_spec.rb
@@ -18,5 +18,28 @@ describe Level do
 
       things.each { |e| e.level.should eq instance }
     end
+
+    it "accepts a representative hash" do
+      class SubThingA < Thing; end
+      class SubThingB < Thing; end
+
+      things_hash = {
+        SubThingA: [50, 50],
+        SubThingB: [
+          [25, 25],
+          [75, 75]
+        ]
+      }
+
+      instance.things = things_hash
+
+      expect(instance.things[0]).to be_instance_of SubThingA
+      expect(instance.things[1]).to be_instance_of SubThingB
+      expect(instance.things[2]).to be_instance_of SubThingB
+
+      expect(instance.things[0].position).to eq V[50, 50]
+      expect(instance.things[1].position).to eq V[25, 25]
+      expect(instance.things[2].position).to eq V[75, 75]
+    end
   end
 end
