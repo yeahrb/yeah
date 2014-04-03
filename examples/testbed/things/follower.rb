e17e949ab3b77885e33befcad521edee743c38ca
Artur Ostrega <skoofoo@gmail.com>
2014-04-03 10:53:41 -0500

    testbed minor cleanup + todo

--- examples/testbed/things/follower.rb
+++ examples/testbed/things/follower.rb
@@ -1,7 +1,7 @@
 class Follower < Thing
   def update
     self.position = mouse.position
-    self.position.x -= size.x / 2
+    self.position.x -= size.x / 2 # TODO: use anchor for this sort of thing
   end
 
   def charged?
--- examples/testbed/visuals/follower_visual.rb
+++ examples/testbed/visuals/follower_visual.rb
@@ -1,10 +1,10 @@
 class FollowerVisual < Box
-  self.size = V[24, 48]
-
   COLORS = {
     default: Color[0.8, 0.5, 0.3], # orange
     charged: Color[0.3, 0.5, 0.8] # blue
   }
+
+  self.size = V[24, 48]
 
   def color
     thing.charged? ? COLORS[:charged] : COLORS[:default]
