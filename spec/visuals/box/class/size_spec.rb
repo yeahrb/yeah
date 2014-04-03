d91b7f3f690312086e29a2a0134f0a49e3a7bcef
Artur Ostrega <skoofoo@gmail.com>
2014-04-02 22:50:38 -0500

    removed redundant Box size specs

--- spec/visuals/box/class/size_spec.rb
+++
@@ -1,8 +0,0 @@
-describe Box do
-  subject { Class.new(described_class) }
-
-  include_examples :has_accessor, :size do
-    let(:default) { nil }
-    let(:assignables) { [V[1, 2, 3]] }
-  end
-end
--- spec/visuals/box/size_spec.rb
+++
@@ -1,6 +0,0 @@
-describe Box do
-  include_examples :has_accessor, :size do
-    let(:default) { V[] }
-    let(:assignables) { [V[9, 5, 3]] }
-  end
-end
