3f123bd772dccb0b641b85dbc2efda8ed299f4c6
Artur Ostrega <skoofoo@gmail.com>
2014-04-03 23:59:17 -0500

    updated testbed example to allow for benchmarking

--- examples/testbed/things/follower.rb
+++ examples/testbed/things/follower.rb
@@ -2,6 +2,13 @@ class Follower < Thing
   def update
     self.position = mouse.position
     self.position.x -= size.x / 2 # TODO: use anchor for this sort of thing
+
+    if mouse.left_clicked? && !@clicking
+      @clicking = true
+      Wanderer.new(space: space, position: position)
+    end
+
+    @clicking = false unless mouse.left_clicked?
   end
 
   def charged?
