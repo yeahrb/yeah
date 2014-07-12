d0ede5becf2c8d1b6351f521c853685f182f5175
Artur Ostrega <skoofoo@gmail.com>
2014-07-12 01:26:53 -0500

    Game::subclasses, Game#setup has no args

--- lib/yeah/game.rb
+++ lib/yeah/game.rb
@@ -2,18 +2,28 @@ module Yeah
 class Game
   attr_reader :display, :keyboard, :mouse
 
+  def self.inherited(klass)
+    self.subclasses << klass
+
+    super
+  end
+
+  def self.subclasses
+    @@subclasses ||= []
+  end
+
   def initialize(args = {})
     @ticker = args.fetch(:ticker)
     @display = args.fetch(:display)
     @keyboard = args.fetch(:keyboard)
     @mouse = args.fetch(:mouse)
 
-    setup(args)
+    setup
   end
 
   private
 
-  def setup(args)
+  def setup
     # overridable noop
   end
 end
