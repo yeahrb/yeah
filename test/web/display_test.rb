28fa1f8a71338e732c73c7525341001d6be35b81
Artur Ostrega <skoofoo@gmail.com>
2014-08-10 19:18:30 -0500

    brought web tests up to date

--- test/web/display_test.rb
+++ test/web/display_test.rb
@@ -13,32 +13,32 @@ class DisplayTest < Test
   end
 
   def test_implements_display_interface
-    methods = %i[size size= fill fill= color_at rectangle]
+    methods = %i[size size= fill_color fill_color= color_at fill_rectangle]
     methods.each { |m| assert_respond_to(@object, m) }
   end
 
   def test_color_at_gets_color_at_position
     position = V[5, 5]
 
-    @object.fill = C[0, 128, 255]
-    @object.rectangle(position, V[1, 1])
+    @object.fill_color = C[0, 128, 255]
+    @object.fill_rectangle(position, V[1, 1])
 
-    assert_equal(@object.color_at(position), @object.fill)
+    assert_equal(@object.color_at(position), @object.fill_color)
   end
 
-  def test_rectangle_fills_area_with_color
+  def test_fill_rectangle_fills_area_with_color
     position = V[100, 200]
     size =     V[100, 100]
 
-    @object.fill = C[255, 128, 0]
-    @object.rectangle(position, size)
+    @object.fill_color = C[255, 128, 0]
+    @object.fill_rectangle(position, size)
 
     topleft = position
     middle = V[position.x + size.x / 2, position.y + size.y / 2]
     bottomright = V[position.x + size.x - 1, position.y + size.y - 1]
 
     [topleft, middle, bottomright].each do |position|
-      assert_equal(@object.fill.value, @object.color_at(position).value)
+      assert_equal(@object.fill_color.value, @object.color_at(position).value)
     end
   end
 end
