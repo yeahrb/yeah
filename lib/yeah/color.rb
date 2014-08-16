054aa9a02656ffa5892d470bb040be4435c5cf4d
Artur Ostrega <skoofoo@gmail.com>
2014-08-16 17:21:44 -0500

    Color inits w/ short hex string

--- lib/yeah/color.rb
+++ lib/yeah/color.rb
@@ -9,8 +9,12 @@ class Color
   attr_reader :value
 
   def initialize(*args)
-    if args.first.respond_to?(:[]) && args.first[0] == '#' # hex string
-      @value = args.first[1..6].scan(/../).map { |h| h.to_i(16) }
+    if args[0].respond_to?(:[]) && args[0][0] == '#' # hex string
+      if args[0].length == 4 # short hex
+        @value = args[0][1..3].chars.map { |h| "#{h}#{h}".to_i(16) }
+      else # normal hex
+        @value = args[0][1..6].scan(/../).map { |h| h.to_i(16) }
+      end
     else
       @value = args
     end
