c289f8379610a62b057fa573077cdf27312c057c
Artur Ostrega <skoofoo@gmail.com>
2014-08-16 21:12:34 -0500

    Game::config

--- lib/yeah/game.rb
+++ lib/yeah/game.rb
@@ -1,16 +1,27 @@
 module Yeah
 class Game
+  class << self
+    def inherited(klass)
+      subclasses << klass
+
+      super
+    end
+
+    def subclasses
+      @@subclasses ||= []
+    end
+
+    def config
+      @config ||= {
+        ticker: {},
+        display: {},
+        keyboard: {},
+        mouse: {}
+      }
+    end
+  end
+
   attr_reader :ticker, :display, :keyboard, :mouse
-
-  def self.inherited(klass)
-    self.subclasses << klass
-
-    super
-  end
-
-  def self.subclasses
-    @@subclasses ||= []
-  end
 
   def initialize(args = {})
     args = defaults.merge(args)
@@ -27,14 +38,18 @@ class Game
   private
 
   def defaults
-    ticker = Ticker.new
+    ticker = Ticker.new(config[:ticker])
 
     {
       ticker: ticker,
-      display: Display.new,
-      keyboard: Keyboard.new(ticker: ticker),
-      mouse: Mouse.new(ticker: ticker)
+      display: Display.new(config[:display]),
+      keyboard: Keyboard.new({ ticker: ticker }.merge(config[:keyboard])),
+      mouse: Mouse.new({ ticker: ticker }.merge(config[:mouse]))
     }
+  end
+
+  def config
+    self.class.config
   end
 
   def setup
