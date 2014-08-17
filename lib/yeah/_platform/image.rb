b2faad1c9c70349ab1ba9891748651340d849109
Artur Ostrega <skoofoo@gmail.com>
2014-08-17 18:40:53 -0500

    image docs

---
+++ lib/yeah/_platform/image.rb
@@ -0,0 +1,17 @@
+module Yeah
+module Platform
+
+# An `Image` is an asset that a `Display` can draw.
+# @abstract Provided by a `Platform`.
+class Image < Asset
+  # @!attribute [r] size
+  # @return [Vector] size of image
+
+  # @!attribute [r] width
+  # @return [Integer] width of image
+
+  # @!attribute [r] height
+  # @return [Integer] height of image
+end
+end
+end
