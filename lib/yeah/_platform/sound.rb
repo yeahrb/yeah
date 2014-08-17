9a7cda2785f2d15d7dce5ad22ed2d357123bd33a
Artur Ostrega <skoofoo@gmail.com>
2014-08-17 18:25:08 -0500

    sound: #pause, docs

---
+++ lib/yeah/_platform/sound.rb
@@ -0,0 +1,17 @@
+module Yeah
+module Platform
+
+# A `Sound` is an audio asset, which can be used to provide music and sound
+# effects for a game.
+# @example Playing a sound
+#   Sound.new('sounds/neigh.wav').play
+# @abstract Provided by a `Platform`.
+class Sound < Asset
+  # @!method play
+  # Play sound.
+
+  # @!method pause
+  # Pause sound. A following call to {#play} will resume.
+end
+end
+end
--- opal/yeah/web/sound.opal
+++ opal/yeah/web/sound.opal
@@ -2,13 +2,17 @@ module Yeah
 module Web
 class Sound < Asset
   def play
-    `#@native.play();`
+    `#@native.play()`
+  end
+
+  def pause
+    `#@native.pause()`
   end
 
   private
 
   def setup_native
-   ` #@native = new Audio();`
+   ` #@native = new Audio()`
   end
 end
 end
