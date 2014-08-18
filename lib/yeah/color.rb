95e81d363628ece1f541d36b7a1d88b3505b316e
Artur Ostrega <skoofoo@gmail.com>
2014-08-17 20:21:21 -0500

    color: docs & minor improvement

--- lib/yeah/color.rb
+++ lib/yeah/color.rb
@@ -1,13 +1,30 @@
 module Yeah
+
+# A `Color` represents a color.
+#
+# `C` is an alias for `Color`.
+# @example Comparing two colors
+#   C[100, 100, 100] == C['#646464']
+#   # => true
 class Color
   class << self
+    # @param arguments catch-all
+    # @return [Vector]
+    # Alias for ::new.
     def [](*args)
       new(*args)
     end
   end
 
+  # @return [Array] color value in RGB format
   attr_reader :value
 
+  # @overload initialize(red, green, blue)
+  #   @param [Numeric] red value, from 0 to 255
+  #   @param [Numeric] green value, from 0 to 255
+  #   @param [Numeric] blue value, from 0 to 255
+  # @overload initialize(hex_string)
+  #   @param [String] hex string in '#dadada' or '#fff' format
   def initialize(*args)
     if args[0].respond_to?(:[]) && args[0][0] == '#' # hex string
       if args[0].length == 4 # short hex
@@ -20,14 +37,16 @@ class Color
     end
   end
 
+  # @return [Boolean] whether self matches other color
   def ==(other)
     value == other.value
   end
 
-  def to_s
-    "#<#{self.class.name} #{value.to_s}>"
+  def inspect
+    "#{self.class.name}#{value.to_s}"
   end
 
+  # @return [String] color value as a hex string
   def to_hex
     "##{value.map { |v| v.to_s(16).rjust(2, '0') }.join }"
   end
