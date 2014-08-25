136dd3e4523ba8f697fec014da8482f1f8aeb8ae
Artur Ostrega <skoofoo@gmail.com>
2014-08-25 00:28:24 -0500

    font docs abstract note

--- lib/yeah/_platform/font.rb
+++ lib/yeah/_platform/font.rb
@@ -2,6 +2,7 @@ module Yeah
 
 # A `Font` is an asset that is used to set `Display#text_font` in order to be
 # drawn using `Display#fill_text` and `Display#stroke_text`.
+# @abstract Provided by a `Platform`.
 # @see Yeah::Display#text_font
 class Font < Asset
 end
