4592296e7971119b97e208adc3f244c9177adc03
Artur Ostrega <skoofoo@gmail.com>
2014-09-13 13:51:22 -0500

    display interface test added transform methods

--- test/display_test.rb
+++ test/display_test.rb
@@ -4,17 +4,25 @@ class DisplayTest < Test
   end
 
   def test_implements_display_interface
-    methods = %i[color_at translate scale rotate push pop stroke_line
-      stroke_rectangle fill_rectangle stroke_ellipse fill_ellipse clear
-      begin_shape end_shape move_to line_to curve_to curve2_to stroke_shape
-      fill_shape image image_cropped fill_text stroke_text]
+    methods = %i[color_at push pop stroke_line stroke_rectangle fill_rectangle
+      stroke_ellipse fill_ellipse clear begin_shape end_shape move_to line_to
+      curve_to curve2_to stroke_shape fill_shape image image_cropped fill_text
+      stroke_text]
     methods.each { |m| assert_respond_to(@object, m) }
 
-    properties = %i[size width height fill_color stroke_color stroke_width
+    transform_methods = %i[translate scale rotate]
+    transform_methods.each do |transform_method|
+      assert_respond_to(@object, transform_method)
+      assert_respond_to(@object, "#{transform_method}_x")
+      assert_respond_to(@object, "#{transform_method}_y")
+      assert_respond_to(@object, "#{transform_method}_z")
+    end
+
+    attributes = %i[size width height fill_color stroke_color stroke_width
       text_font text_size]
-    properties.each do |property|
-      assert_respond_to(@object, property)
-      assert_respond_to(@object, "#{property}=")
+    attributes.each do |attribute|
+      assert_respond_to(@object, attribute)
+      assert_respond_to(@object, "#{attribute}=")
     end
   end
 
