efb4930805c53dc1ead58ed98fde630cf46e26e4
Artur Ostrega <artur.mariusz.ostrega@gmail.com>
2014-10-21 20:59:49 -0500

    fixed Vec2 #length & #distance_to, #equals? -> #equal?

--- lib/yeah/vec2.rb
+++ lib/yeah/vec2.rb
@@ -3,53 +3,53 @@ module Yeah
 # The `Vec2` module provides methods for manipulating 2-dimensional vectors.
 # A vector is represented by a sequence of Numeric component values.
 module Vec2
-  module_function
+  class << self
+    def equal?(ax, ay, bx, by)
+      ax == bx && ay == by
+    end
 
-  def equals?(ax, ay, bx, by)
-    ax == bx && ay == by
-  end
+    def magnitude(x, y)
+      Math.sqrt(x ** 2 + y ** 2)
+    end
 
-  def magnitude(x, y)
-    Math.sqrt(x ** 2 + y ** 2)
-  end
+    def direction(x, y)
+      Math.atan2(y, x)
+    end
 
-  def direction(x, y)
-    Math.atan2(y, x)
-  end
+    alias :length :magnitude
 
-  alias :length :magnitude
+    def normalize(x, y)
+      magnitude = magnitude(x, y)
+      [x / magnitude, y / magnitude]
+    end
 
-  def normalize(x, y)
-    magnitude = magnitude(x, y)
-    [x / magnitude, y / magnitude]
-  end
+    def add(ax, ay, bx, by)
+      [ax + bx, ay + by]
+    end
 
-  def add(ax, ay, bx, by)
-    [ax + bx, ay + by]
-  end
+    def subtract(ax, ay, bx, by)
+      [ax - bx, ay - by]
+    end
 
-  def subtract(ax, ay, bx, by)
-    [ax - bx, ay - by]
-  end
+    def multiply(ax, ay, b)
+      [ax * b, ay * b]
+    end
 
-  def multiply(ax, ay, b)
-    [ax * b, ay * b]
-  end
+    def divide(ax, ay, b)
+      [ax / b, ay / b]
+    end
 
-  def divide(ax, ay, b)
-    [ax / b, ay / b]
-  end
+    def dot_product(ax, ay, bx, by)
+      ax * bx + ay * by
+    end
 
-  def dot_product(ax, ay, bx, by)
-    ax * bx + ay * by
-  end
+    def distance_to(ax, ay, bx, by)
+      Math.sqrt((ax - bx) ** 2 + (ay - by) ** 2)
+    end
 
-  def distance_to(ax, ay, bx, by)
-    Math.sqrt((ax - bx) ** 2, (ay - by) ** 2)
-  end
-
-  def direction_to(ax, ay, bx, by)
-    Math.atan2(by - ay, bx - by)
+    def direction_to(ax, ay, bx, by)
+      Math.atan2(by - ay, bx - by)
+    end
   end
 end
 
