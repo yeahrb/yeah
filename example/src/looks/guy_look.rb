0743bfb664d1aa669e6e916b136761dd7d67bd8b
Artur Ostręga <artmarost@gmail.com>
2016-02-14 18:55:05 -0600

    example: Set GuyLook::speed

--- example/src/looks/guy_look.rb
+++ example/src/looks/guy_look.rb
@@ -2,6 +2,7 @@ class GuyLook < Yeah::SpriteLook #
 #class GuyLook < SpriteLook
   self.image = Yeah::Image['guy.png'] #
   self.size = 64, 64
+  self.speed = 15
 
   self.animations = {
     stand_up: 0,
