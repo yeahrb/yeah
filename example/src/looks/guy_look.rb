e7bebaa804ca4f8f56a67acfa9b0053e24238a38
Artur Ostręga <artmarost@gmail.com>
2016-02-16 04:02:17 -0600

    example: Define GuyLook#animation

--- example/src/looks/guy_look.rb
+++ example/src/looks/guy_look.rb
@@ -1,5 +1,7 @@
 class GuyLook < Yeah::SpriteLook #
 #class GuyLook < SpriteLook
+  attr_accessor :walking, :direction
+
   self.image = Yeah::Image['guy.png'] #
   self.size = 64, 64
   self.speed = 15
@@ -14,4 +16,32 @@ class GuyLook < Yeah::SpriteLook #
     walk_down: (18..26),
     walk_right: (27..35)
   }
+
+  def animation
+    if walking
+      case direction
+      when :right
+        :walk_right
+      when :left
+        :walk_left
+      when :up
+        :walk_up
+      when :down
+        :walk_down
+      end
+    else
+      case direction
+      when :right
+        :stand_right
+      when :left
+        :stand_left
+      when :up
+        :stand_up
+      when :down
+        :stand_down
+      else
+        :stand_down
+      end
+    end
+  end
 end
--- example/src/things/guy.rb
+++ example/src/things/guy.rb
@@ -5,39 +5,31 @@ class Guy < Yeah::Thing
   self.look = GuyLook #
 
   def act(elapsed)
-    walking = false
+    look.walking = false
 
     if keyboard.pressing? :right
       self.x += speed * elapsed
-      look.animation = :walk_right if keyboard.pressed? :right
-      walking = true
+      look.direction = :right
+      look.walking = true
     end
 
     if keyboard.pressing? :left
       self.x -= speed * elapsed
-      look.animation = :walk_left if keyboard.pressed? :left
-      walking = true
+      look.direction = :left
+      look.walking = true
     end
 
     if keyboard.pressing? :up
       self.y += speed * elapsed
-      look.animation = :walk_up if keyboard.pressed? :up
-      walking = true
+      look.direction = :up
+      look.walking = true
     end
 
     if keyboard.pressing? :down
       self.y -= speed * elapsed
-      look.animation = :walk_down if keyboard.pressed? :down
-      walking = true
+      look.direction = :down
+      look.walking = true
     end
-
-    unless walking
-      look.animation = :stand_right if keyboard.released? :right
-      look.animation = :stand_left if keyboard.released? :left
-      look.animation = :stand_up if keyboard.released? :up
-      look.animation = :stand_down if keyboard.released? :down
-    end
-
   end
 
   def speed
