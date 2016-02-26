e64ee0e80156441f94b057569bb70c20e4f482e3
Artur Ostręga <artmarost@gmail.com>
2016-02-26 03:45:20 -0600

    example: Create space's #things using ::things

--- example/src/spaces/world.rb
+++ example/src/spaces/world.rb
@@ -9,28 +9,15 @@ class World < Yeah::Space #
 
   self.things = {
     Cloud => [
-      -> { [width * 0.6, height * 0.8] },
-      -> { [width * 0.2, height * 0.7] },
-      -> { [width * 0.1, height * 0.9] }
+      [384, 288],
+      [128, 252],
+      [64, 324]
     ],
-
     Duck => [
       [10, 10]
     ],
-
     Guy => [
-      -> { [width * 0.8, height * 0.8] }
+      [512, 288]
     ]
   }
-
-  def initialize(game)
-    super
-
-    things << Cloud.new(game, x: width * 0.6, y: height * 0.8)
-    things << Cloud.new(game, x: width * 0.2, y: height * 0.7)
-    things << Cloud.new(game, x: width * 0.1, y: height * 0.9)
-    things << Duck.new(game, x: 10, y: 10)
-    things << Guy.new(game, x: width * 0.8, y: height * 0.8)
-    #things << Duck.new(game, position: Vec2.divide(size, 2))
-  end
 end
